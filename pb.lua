a9ff188da2528e4cb16da97880d170f8afc451b6
Iamiakk <153729760+Iamiak@users.noreply.github.com>
2024-05-12 10:16:31 +0530

    Update pb.lua

--- pb.lua
+++ pb.lua
@@ -952,49 +952,8 @@ local function CIVJWIK_fake_script() -- TextButton_11.LocalScript
 	end)
 end
 coroutine.wrap(CIVJWIK_fake_script)()
-local function YLRZFHD_fake_script() -- TextButton_12.LocalScript 
-	local script = Instance.new('LocalScript', TextButton_12)
-
-	script.Parent.MouseButton1Click:Connect(function()
-		
-		privateProperties = { --customize private logs
-			Color = Color3.fromRGB(0,255,255); 
-			Font = Enum.Font.SourceSansBold;
-			TextSize = 18;
-		}
-	
-	
-		local StarterGui = game:GetService("StarterGui")
-		local Players = game:GetService("Players")
-		local player = Players.LocalPlayer or Players:GetPropertyChangedSignal("LocalPlayer"):Wait() or Players.LocalPlayer
-		local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
-		local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
-		local instance = (_G.chatSpyInstance or 0) + 1
-		_G.chatSpyInstance = instance
-	
-		local function onChatted(p,msg)
-			local conn = getmsg.OnClientEvent:Connect(function(packet,channel)
-				if packet.SpeakerUserId==p.UserId and packet.Message==msg:sub(#msg-#packet.Message+1) and (channel=="All" or (channel=="Team" and public==false and Players[packet.FromSpeaker].Team==player.Team)) then
-					hidden = false
-				end
-			end)
-			wait(1)
-			conn:Disconnect()
-			if hidden and enabled then
-				if public then
-					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} [".. p.Name .."]: "..msg,"All")
-				else
-					privateProperties.Text = "{SPY} [".. p.Name .."]: "..msg
-					StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
-				end
-			end
-	end) 
-	StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
-	if not player.PlayerGui:FindFirstChild("Chat") then wait(3) end
-	local chatFrame = player.PlayerGui.Chat.Frame
-	chatFrame.ChatChannelParentFrame.Visible = true
-	chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
-	end)
+local function YLRZFHD_fake_script()
+	print("hlo") -- TextButton_12.LocalScript 
 end
 coroutine.wrap(YLRZFHD_fake_script)()
 local function XCRQSF_fake_script() -- TextButton_13.LocalScript 
